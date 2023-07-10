265cc641759121916c9bccd10a1fc8f0cda02f93
Basem M. Gaber <basem.gaber@incorta.com>
2023-07-10 10:52:59 +0300

    Dynamic commands placement in jinja template

--- opencopilot/handlers/executor/gpt_task_processor.py
+++ opencopilot/handlers/executor/gpt_task_processor.py
@@ -7,7 +7,8 @@ from opencopilot.utils import logger, exceptions
 from opencopilot.utils.exceptions import UnknownCommandError
 from opencopilot.utils.open_ai import completion_3_5, completion_4
 
-operators_handler_module = importlib.import_module(operators_path + ".operators_handler")
+operators_handler_module = importlib.import_module(
+    operators_path + ".operators_handler")
 
 
 def get_command_prompt_from_task(query_str, tasks, task_index, target="PLANNER"):
@@ -15,9 +16,11 @@ def get_command_prompt_from_task(query_str, tasks, task_index, target="PLANNER")
 
     # Check that the task's operator exists in the operators' group op_functions and get its command help
     if task[constants.Operator] in operators_handler_module.op_functions:
-        commands_help = operators_handler_module.op_functions[task[constants.Operator]]["get_commands_help"]()
+        commands_help = operators_handler_module.op_functions[task[constants.Operator]]["get_commands_help"](
+        )
     else:
-        raise exceptions.UnknownCommandError(f"Unknown command operator: {task[constants.Operator]}")
+        raise exceptions.UnknownCommandError(
+            f"Unknown command operator: {task[constants.Operator]}")
 
     sub_tasks_expectations = None
     if "expected_sub_tasks_count" in commands_help:
@@ -40,8 +43,7 @@ def get_command_prompt_from_task(query_str, tasks, task_index, target="PLANNER")
         "tasks": json.dumps(tasks[:tasks_count], indent=2),
         "curTaskId": task["id"],
         "commands_overview": commands_help["overview"],
-        "commands_command1_description": json.dumps([commands_help["commands"][0]["command_description"]], indent=2),
-        "commands_command1": json.dumps([commands_help["commands"][0]["command"]], indent=2),
+        "commands": commands_help["commands"],
         "tasksLength": tasks_count,
         "sub_tasks_expectations": sub_tasks_expectations,
         "service_name": operators_handler_module.group_name
@@ -51,26 +53,30 @@ def get_command_prompt_from_task(query_str, tasks, task_index, target="PLANNER")
 
 
 def get_command_from_task(query_str, tasks, task_index, session_entry):
-    prompt_text = get_command_prompt_from_task(query_str, tasks, task_index, "EXECUTOR")
+    prompt_text = get_command_prompt_from_task(
+        query_str, tasks, task_index, "EXECUTOR")
 
     logger.system_message("Creating command from task description")
     messages = [{"role": "system", "content": prompt_text}]
     logger.print_gpt_messages(messages)
 
-    session_entry.set_pending_agent_communications(component=task_index, sub_component="request", value=messages)
+    session_entry.set_pending_agent_communications(
+        component=task_index, sub_component="request", value=messages)
 
     """ If get_op_command is enabled, retrieve operator's command from sessions_store instead of requesting it from GPT """
     command = None
     if env.sessions_getting_mode and (env.get_all or env.get_op_command):
-        command = session_entry.get_cached_agent_communications(component=task_index, sub_component=constants.Command)
+        command = session_entry.get_cached_agent_communications(
+            component=task_index, sub_component=constants.Command)
 
-    preferred_LLM = operators_handler_module.op_functions[tasks[task_index]["operator"]]["preferred_LLM"]
+    preferred_LLM = operators_handler_module.op_functions[tasks[task_index]
+                                                          ["operator"]]["preferred_LLM"]
     if command is None:
         if preferred_LLM == "GPT-4":
             logger.system_message("Calling ChatGPT {preferred_LLM}:")
             chat_gpt_response = completion_4.run(
-                    messages
-                )
+                messages
+            )
         else:
             logger.system_message("Calling ChatGPT GPT-3.5:")
             chat_gpt_response = completion_3_5.run(
@@ -93,14 +99,16 @@ def enhance_and_finalize_task_result(command, task, task_index, session_entry):
         "initialCommand": json.dumps(command)
     })
 
-    logger.system_message("Calling ChatGPT 3.5, to fine tune, and finalize task result")
+    logger.system_message(
+        "Calling ChatGPT 3.5, to fine tune, and finalize task result")
     messages = [{"role": "system", "content": prompt_text}]
     logger.print_gpt_messages(messages)
 
     """ If get_op_enhanced_result is enabled, retrieve operator's enhanced_result from sessions_store instead of requesting it from GPT """
     chat_gpt_response = None
     if env.sessions_getting_mode and (env.get_all or env.get_op_enhanced_result):
-        chat_gpt_response = session_entry.get_cached_agent_communications(component=task_index, sub_component=constants.EnhancedResult)
+        chat_gpt_response = session_entry.get_cached_agent_communications(
+            component=task_index, sub_component=constants.EnhancedResult)
 
     if chat_gpt_response is None:
         chat_gpt_response = completion_3_5.run(
