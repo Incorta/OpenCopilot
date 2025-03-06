6deef3aef2db9f7bfdd0c9fd7c9de99f622c11d7
HaniaMoHani <hania.anwar@incorta.com>
2025-03-06 17:40:49 +0200

    Refactor llm model object access

--- opencopilot/utils/langchain/llm_GPT.py
+++ opencopilot/utils/langchain/llm_GPT.py
@@ -10,47 +10,15 @@ from langchain.schema import (
     SystemMessage
 )
 import opencopilot.utils.logger as logger
-from opencopilot.configs.LLM_Configurations import LLMConfigurations
 from opencopilot.configs.ai_providers import SupportedAIProviders, get_model_temperature
 from opencopilot.utils.consumption_tracker import ConsumptionTracker
 from opencopilot.utils.exceptions import APIFailureException, UnsupportedAIProviderException, LLMException
-from opencopilot.configs.constants import LLMModelPriority
 from langchain_community.callbacks import get_openai_callback
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 from opencopilot.utils.utilities import extract_json_block
 
-llm_configs = None
 LLM_RETRY_COUNT = 3
-
-
-def initialize_configurations():
-    global llm_configs
-    llm_configs = LLMConfigurations.execute()
-
-
-def update_configurations(key1, value1, key2=None, value2=None):
-    llm_configs[LLMModelPriority.primary_model.value][key1] = value1
-    if key2 is not None:
-        llm_configs[LLMModelPriority.secondary_model.value][key2] = value2
-
-
-def resolve_llm_model(llm_names, priority_list_mode=True):
-    model = None
-    if priority_list_mode:
-        # Select the first found configured model
-        for llm_model in llm_names:
-            if llm_model in llm_configs and "ai_provider" in llm_configs[llm_model]:
-                model = llm_configs[llm_model]
-                break
-    else:
-        # Resolve extra predefined model
-        model = llm_names
-
-    if model is None:
-        raise UnsupportedAIProviderException("Didn't find configurations of any of the desired models, "
-                                             "Please set the configuration of the desired model in the env!")
-    return model
 
 
 def run(messages, model):
@@ -100,36 +68,36 @@ def run(messages, model):
 
 def get_llm(model):
     logger.info(model)
-    if model["ai_provider"] == SupportedAIProviders.openai.value["provider_name"]:
+    if model.ai_provider == SupportedAIProviders.openai.value["provider_name"]:
         return ChatOpenAI(
-            api_key=model["openai_api_text_completion_key"],
-            base_url=model.get("openai_api_text_completion_baseurl"),
-            model=model["openai_api_text_completion_model_name"],
-            temperature=get_model_temperature(model["ai_provider"]),
+            api_key=model.api_key,
+            base_url=model.api_base_url,
+            model=model.model_name,
+            temperature=get_model_temperature(model.ai_provider),
             max_tokens=4096,
-        ), model["openai_api_text_completion_model_name"]
-    elif model["ai_provider"] == SupportedAIProviders.azure_openai.value["provider_name"]:
+        ), model.model_name
+    elif model.ai_provider == SupportedAIProviders.azure_openai.value["provider_name"]:
         return AzureChatOpenAI(
-            openai_api_key=model["azure_openai_text_completion_key"],
+            openai_api_key=model.api_key,
             openai_api_version="2023-05-15",
-            azure_endpoint=model["azure_openai_text_completion_endpoint"],
-            deployment_name=model["azure_openai_text_completion_deployment_name"],
-            temperature=get_model_temperature(model["ai_provider"]),
-        ), model["azure_openai_text_completion_deployment_name"]
-    elif model["ai_provider"] == SupportedAIProviders.google_gemini.value["provider_name"]:
+            azure_endpoint=model.api_base_url,
+            deployment_name=model.model_name,
+            temperature=get_model_temperature(model.ai_provider),
+        ), model.model_name
+    elif model.ai_provider == SupportedAIProviders.google_gemini.value["provider_name"]:
         return ChatGoogleGenerativeAI(
-            model=model["google_gemini_text_completion_model_name"],
-            google_api_key=model["google_gemini_text_completion_key"],
-            temperature=get_model_temperature(model["ai_provider"]),
+            model=model.model_name,
+            google_api_key=model.api_key,
+            temperature=get_model_temperature(model.ai_provider),
             convert_system_message_to_human=True
-        ), model["google_gemini_text_completion_model_name"]
-    elif model["ai_provider"] == SupportedAIProviders.aixplain.value["provider_name"]:
-        os.environ["TEAM_API_KEY"] = model["aixplain_text_completion_key"]
+        ), model.model_name
+    elif model.ai_provider == SupportedAIProviders.aixplain.value["provider_name"]:
+        os.environ["TEAM_API_KEY"] = model.api_key
         from opencopilot.utils.langchain.aixplain import AixplainChatModel
         return AixplainChatModel(
-            model_id=model["aixplain_text_completion_model_id"],
-            temperature=get_model_temperature(model["ai_provider"]),
+            model_id=model.model_name,
+            temperature=get_model_temperature(model.ai_provider),
             max_tokens=4096
-        ), model["aixplain_text_completion_model_id"]
+        ), model.model_name
     else:
         raise UnsupportedAIProviderException("Unsupported AI Provider")
