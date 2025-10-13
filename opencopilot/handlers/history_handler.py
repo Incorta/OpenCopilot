69ae8398fcae7e99a8a5847ca85df91260d8a860
Karim Alaa <karim.alaa@incorta.com>
2025-10-13 22:16:45 +0300

    Add inapplicable question exception in history

--- opencopilot/handlers/history_handler.py
+++ opencopilot/handlers/history_handler.py
@@ -9,6 +9,9 @@ from opencopilot.configs.env import operators_path, service_utils_path
 from opencopilot.controller.executors_factory import ExecutorsFactory
 from opencopilot.utils.tokens_counter import count_prompt_tokens
 from opencopilot.utils import logger
+from service.controller.session_handler import QueryState
+from service.controller.tasks import TaskStatus
+from service.utils.exceptions import InapplicableQuestionException
 
 operators_handler_module = importlib.import_module(operators_path + ".operators_handler")
 service_utils_constant_module = importlib.import_module(service_utils_path + ".constants")
@@ -47,7 +50,7 @@ def summarize_session_queries(user_session, max_history_size=service_utils_const
     for query in user_session.queries_list[:-1]:
         # Include only successful queries in history
         state = query.get_state()
-        if state != "SUCCESS":  # TODO clean it to use the QueryState ENUM
+        if state not in (QueryState.SUCCESS, QueryState.EXCEPTION):  # TODO clean it to use the QueryState ENUM
             continue
 
         query_msg = query.get_user_query_msg()
@@ -57,26 +60,36 @@ def summarize_session_queries(user_session, max_history_size=service_utils_const
         # Check if there are any tasks
         if tasks:
             last_task = tasks[-1]
-            operator_name = last_task.get(Operator, "")
-            operator = operators_handler_module.op_functions_resolver(context)[operator_name]
-            try:
-                op_executor = ExecutorsFactory().get_op_executor(operator)
-                query_result = ""
-                if query.get_layout_renderer():
-                    query_result = query.get_layout_renderer().generate_summary_from_tasks(copy.deepcopy(tasks), context)
-                else:
-                    query_result = op_executor.prepare_history_object(operator, copy.deepcopy(tasks))
+            if state == QueryState.SUCCESS:
+                operator_name = last_task.get(Operator, "")
+                operator = operators_handler_module.op_functions_resolver(context)[operator_name]
+                try:
+                    op_executor = ExecutorsFactory().get_op_executor(operator)
+                    query_result = ""
+                    if query.get_layout_renderer():
+                        query_result = query.get_layout_renderer().generate_summary_from_tasks(copy.deepcopy(tasks), context)
+                    else:
+                        query_result = op_executor.prepare_history_object(operator, copy.deepcopy(tasks))
 
-                # Add the new query and result to the queue
-                if query_result:
-                    summary_queue.append({
-                        "user_query_msg": query_msg,
-                        "reply": query_result
-                    })
-                    query_length = count_prompt_tokens(query_msg) + count_prompt_tokens(query_result)
-                    total_length += query_length  # Update the total length counter
-            except Exception as e:
-                logger.error("Couldn't get history object for the previous task" + str(e))
+                    # Add the new query and result to the queue
+                    if query_result:
+                        summary_queue.append({
+                            "user_query_msg": query_msg,
+                            "reply": query_result
+                        })
+                        query_length = count_prompt_tokens(query_msg) + count_prompt_tokens(query_result)
+                        total_length += query_length  # Update the total length counter
+                except Exception as e:
+                    logger.error("Couldn't get history object for the previous task" + str(e))
+
+            elif state == QueryState.EXCEPTION:
+                for task in tasks:
+                    if task['status'] == TaskStatus.ERROR and "error" in task and isinstance(query.exception, InapplicableQuestionException):
+                            summary_queue.append({
+                                "user_query_msg": query_msg,
+                                "reply": {"reply": query.exception.message}
+                            })
+            
 
         # If the total length exceeds max_history_size, remove the oldest queries
         while total_length > max_history_size and summary_queue:
